c766c97f7495cadbba708c074a1cbaa734646534
Shun Iwashita <iwashunabc519@gmail.com>
2022-01-14 19:15:43 +0700

    update

--- index.d.ts
+++ index.d.ts
@@ -3,8 +3,12 @@ type functionInteract = ( providedObject: { id: (string | number), others: any }
 
 declare class SimpleCRUD {
    constructor(requiredProps?: string[], strictMode?: boolean, defaultValue?: object[]);
-   create: (newObject: { id: (number | string)}) => object;
-   read: (searchProperty?: object) => object[] | object;
+   create: (newObject: { id: (number | string)}) => object = (newObject): object => {
+
+   };
+   read: (searchProperty?: object) => (object[] | object);
 }
+
+declare function test(testObj: { id: number, others: any[]}): string;
 
 export default SimpleCRUD;
